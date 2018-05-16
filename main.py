4f822a19938982149412147cacf8e8672302b8f3
yutongshen <yutongshen213@gmail.com>
2018-05-16 11:22:12 +0800

    Add 'trap'

--- main.py
+++ main.py
@@ -7,11 +7,14 @@ import time
 ACTIONS     = ['up', 'down', 'left', 'right']
 LENGTH      = None
 N_STATES    = None
+START       = None
+HOLE        = None
 TERMINAL    = None
 EPSILON     = None
 MAX_EPISODE = None
 LAMBDA      = None
 ALPHA       = None
+FIRST       = True
 
 # Initial Q-Table
 
@@ -41,6 +44,7 @@ def actor(state, q_table):
 def update_env(state, episode, step):
     view = np.array([['_ '] * LENGTH] * LENGTH)
     view[tuple(TERMINAL)] = '* '
+    view[HOLE] = 'X '
     view[tuple(state)] = 'o '
     interaction = ''
     for v in view:
@@ -49,15 +53,31 @@ def update_env(state, episode, step):
     interaction += message
     if state == TERMINAL:
         print(interaction)
-        time.sleep(.5)
+        time.sleep(.1)
     else:
         print(interaction)
-        time.sleep(.5)
+        time.sleep(.1)
 
 # Enviroment Feedback
 
+def init_env():
+    global HOLE
+    global FIRST
+    global START
+    global TERMINAL
+    start = START
+    if FIRST:
+        f = lambda: np.random.choice(range(LENGTH))
+        hole = f(), f()
+        while hole == START and hole == TERMINAL:
+            hole = f(), f()
+        HOLE = hole
+        FIRST = False
+    return start, False
+
 def get_env_feedback(state, action):
     reward = 0.
+    end = False
     a, b = state
     if action == 'up':
         a -= 1
@@ -66,16 +86,26 @@ def get_env_feedback(state, action):
         next_state = (a, b)
         if next_state == TERMINAL:
             reward = 1.
+            end = True
+        elif next_state == HOLE:
+            reward = -1.
+            end = True
     elif action == 'down':
         a += 1
         if a >= LENGTH:
             a = LENGTH - 1
         next_state = (a, b)
+        if next_state == HOLE:
+            reward = -1.
+            end = True
     elif action == 'left':
         b -= 1
         if b < 0:
             b = 0
         next_state = (a, b)
+        if next_state == HOLE:
+            reward = -1.
+            end = True
     elif action == 'right':
         b += 1
         if b >= LENGTH:
@@ -83,7 +113,11 @@ def get_env_feedback(state, action):
         next_state = (a, b)
         if next_state == TERMINAL:
             reward = 1.
-    return next_state, reward
+            end = True
+        elif next_state == HOLE:
+            reward = -1.
+            end = True
+    return next_state, reward, end
 
 # Run Game
 
@@ -91,14 +125,14 @@ def run():
     q_table = build_q_table()
     episode = 0
     while episode < MAX_EPISODE:
-        state = (LENGTH - 1, 0)
+        state, end = init_env()
         step = 0
         update_env(state, episode, step)
-        while state != TERMINAL:
+        while not end:
             a, b = state
             act = actor(a * LENGTH + b, q_table)
             print(act)
-            next_state, reward = get_env_feedback(state, act)
+            next_state, reward, end = get_env_feedback(state, act)
             na, nb = next_state
             q_predict = q_table.ix[a * LENGTH + b, act]
             if next_state != TERMINAL:
@@ -136,6 +170,7 @@ if __name__ == '__main__':
     
     LENGTH      = int(args.LENGTH)
     N_STATES    = LENGTH * LENGTH
+    START       = (LENGTH - 1, 0)
     TERMINAL    = (0, LENGTH - 1)
     EPSILON     = .9
     MAX_EPISODE = int(args.ITERATION)
